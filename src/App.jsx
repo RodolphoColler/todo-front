c10e8afa0d2512acd1f52139a1c7cc0a0395deda
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-05-19 11:01:47 -0300

    feat: create simple todo base

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+import './todo.css';
+import { BiPlusMedical } from 'react-icons/bi';
+import { useState } from 'react';
+
 function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setTodos((prev) => [...prev, newTodo]);
+  }
+
   return (
-    <h1> fnasd </h1>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="todo-input">
+          <input
+            type="text"
+            id="todo-input"
+            placeholder="write a new todo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+        </label>
+        <button type="submit" aria-label="add new todo"><BiPlusMedical /></button>
+      </form>
+      <ul>
+        {
+          todos.map((todo) => <li>{ todo }</li>)
+        }
+      </ul>
+    </main>
   );
 }
 
